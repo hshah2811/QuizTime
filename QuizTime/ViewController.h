f412e2b7e86b05bb6966cdfdbb2c3a29c807d518
Harshit <csammac@Harshit-Mac-Book.local>
2013-11-20 23:15:41 +0530

    Initial Commit

---
+++ QuizTime/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuizTime
+//
+//  Created by Harshit on 11/20/13.
+//  Copyright (c) 2013 iGEEK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
